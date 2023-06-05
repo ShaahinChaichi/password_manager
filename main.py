a54520b0f24efefeb14ab5b00d0eee8ff0605e12
Shaahin Chaichi <shaahin.chaichi@gmail.com>
2023-06-05 16:33:45 +0330

    UI setup

---
+++ main.py
@@ -0,0 +1,14 @@
+from tkinter import *
+
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Password Manager")
+
+canvas = Canvas(height=200, width=200)
+logo_img = PhotoImage(file="logo.png")
+
+window.mainloop()
