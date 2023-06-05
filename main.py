cd63b84ee7b8bf0cdf1b7c66a84cf8dff3c29ccb
Shaahin Chaichi <shaahin.chaichi@gmail.com>
2023-06-05 21:39:50 +0330

    Save function added

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
 from tkinter import *
+from tkinter import messagebox
+
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save():
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+
+    is_ok = messagebox.askyesno(title=website, message=f"These are details entered \nEmail = {email}"
+                                                       f"\n password: {password}\n"
+                                                       f"is it ok?")
+
+    if is_ok:
+        with open("data.txt", mode="a") as data_file:
+            data_file.writelines(f"{website} | {email} | {password}\n")
+            website_entry.delete(0, END)
+            email_entry.delete(0, END)
+            password_entry.delete(0, END)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -30,7 +48,7 @@ password_entry.grid(row=3, column=1, columnspan=1)
 
 password_button = Button(text="Generate Password")
 password_button.grid(row=3, column=3)
-add_button = Button(text="Add", width=36)
+add_button = Button(text="Add", width=36, command=save)
 add_button.grid(row=4, column=1)
 
 window.mainloop()
