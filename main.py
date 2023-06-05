ef07cda3ffdae7a69102199c7f92301683217b52
Shaahin Chaichi <shaahin.chaichi@gmail.com>
2023-06-05 22:36:56 +0330

    password gen added

--- main.py
+++ main.py
@@ -1,8 +1,34 @@
 from tkinter import *
 from tkinter import messagebox
+import random
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def password_generator():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+               'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+               'u', 'v', 'w', 'x', 'y', 'z']
+
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+    symbols = ['`', '~', '!', '@', '#', '$', '%',
+               '^', '&', '*', '(', ')', '_', '-', '+',
+               '=', '{', '[', '}', '}', '|']
+
+    nr_letters = random.randint(8, 10)
+    nr_numbers = random.randint(2, 4)
+    nr_symbols = random.randint(2, 4)
+
+    password_letters = [random.choice(letters) for _ in range(nr_letters)]
+    password_numbers = [random.choice(numbers) for _ in range(nr_numbers)]
+    password_symbols = [random.choice(symbols) for _ in range(nr_symbols)]
+
+    password_list = password_letters + password_numbers + password_symbols
+    random.shuffle(password_list)
+
+    password = "".join(password_list)
+    print(f"Your password is: {password}")
+
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
@@ -10,16 +36,20 @@ def save():
     email = email_entry.get()
     password = password_entry.get()
 
-    is_ok = messagebox.askyesno(title=website, message=f"These are details entered \nEmail = {email}"
-                                                       f"\n password: {password}\n"
-                                                       f"is it ok?")
+    if len(website) == 0 or len(password) == 0:
+        messagebox.showinfo(title="Info", message="Please dont leave any blank field")
 
-    if is_ok:
-        with open("data.txt", mode="a") as data_file:
-            data_file.writelines(f"{website} | {email} | {password}\n")
-            website_entry.delete(0, END)
-            email_entry.delete(0, END)
-            password_entry.delete(0, END)
+    else:
+        is_ok = messagebox.askyesno(title=website, message=f"These are details entered \nEmail = {email}"
+                                                           f"\n password: {password}\n"
+                                                           f"is it ok?")
+
+        if is_ok:
+            with open("data.txt", mode="a") as data_file:
+                data_file.writelines(f"{website} | {email} | {password}\n")
+                website_entry.delete(0, END)
+                email_entry.delete(0, END)
+                password_entry.delete(0, END)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -46,7 +76,7 @@ email_entry.grid(row=2, column=1, columnspan=1)
 password_entry = Entry(width=21)
 password_entry.grid(row=3, column=1, columnspan=1)
 
-password_button = Button(text="Generate Password")
+password_button = Button(text="Generate Password", command=password_generator)
 password_button.grid(row=3, column=3)
 add_button = Button(text="Add", width=36, command=save)
 add_button.grid(row=4, column=1)
