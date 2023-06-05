db80d2da99ed2f6f57e47fdb841ca749aa8efc4c
Shaahin Chaichi <shaahin.chaichi@gmail.com>
2023-06-05 17:01:18 +0330

    UI setup

--- main.py
+++ main.py
@@ -7,8 +7,18 @@ from tkinter import *
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Password Manager")
+window.config(padx=20, pady=20)
 
 canvas = Canvas(height=200, width=200)
 logo_img = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=logo_img)
+canvas.grid(row=0, column=1)
+
+web_label = Label(text="Website: ")
+web_label.grid(row=1, column=0)
+email_label = Label(text="Email/Username")
+email_label.grid(row=2, column=0)
+pass_label = Label(text="Password")
+pass_label.grid(row=3, column=0)
 
 window.mainloop()
